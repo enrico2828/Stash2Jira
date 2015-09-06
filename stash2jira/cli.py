fcf6f3948b7643b5bafb08ee1da23ffcc8bac586
Enrico Welder <abeltje@gmx.net>
2015-09-06 22:41:23 +0200

    added issue links field, loading up config file correctly

--- stash2jira/cli.py
+++ stash2jira/cli.py
@@ -24,34 +24,34 @@ CONFIG_VARS = [
     {'var': "until", 'error': 'End date not specified'}
 ]
 
-headers = ["key", "issuetype", "status", "fixVersions"]
-
 
 def load_from_config(config_file, args, values):
-    load_config = os.path.join(BASE_CONFIG_DIR, config_file)
-    if os.path.exists(load_config):
-        parser = SafeConfigParser()
-        parser.read(load_config)
-        config = dict()
-        for _var in CONFIG_VARS:
-            config[_var['var']] = parser.get('settings', _var['var'])
-            if not config[_var]:
-                click.echo(_var['error'])
+    config = dict()
+    if config_file:
+        load_config = os.path.join(BASE_CONFIG_DIR, config_file)
+        if os.path.exists(load_config):
+            parser = SafeConfigParser()
+            parser.read(load_config)
+            for _var in CONFIG_VARS:
+                config[_var['var']] = parser.get('settings', _var['var'])
 
-        # TODO: cli args must overwrite config file
+    for _var in CONFIG_VARS:
+        if _var['var'] in args:
+            config[_var['var']] = values[_var['var']]
+        if _var["var"] not in config:
+            click.echo(_var['error'])
+    return config
 
-        return config
 
-
-def save_to_config(config_file, args, values, verbose=False):
+def save_to_config(config_file, config_obj, verbose=False):
     save_config = os.path.join(BASE_CONFIG_DIR, config_file)
     parser = SafeConfigParser()
     parser.read(save_config)
     if not os.path.exists(save_config):
         parser.add_section('settings')
-    for arg in args:
-        if arg in [c['var'] for c in CONFIG_VARS]:
-            parser.set('settings', arg, values[arg])
+    for k, v in config_obj.items():
+        if v is not None:
+            parser.set('settings', k, v)
     with open(save_config, 'w') as f:
         parser.write(f)
         if verbose:
@@ -63,7 +63,9 @@ def get_proxy(proxy_url):
     return {res.scheme: res.geturl()}
 
 
-def get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until):
+# TODO: Inject config file in get_jira_keys
+def get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until, project, repo):
+    stash_url = urljoin(stash_url, 'stash/rest/api/1.0/projects/' + project + '/repos/' + repo + '/commits')
     last_page = False
     start = 0
     jira_keys = list()
@@ -84,7 +86,7 @@ def get_jira_keys(include_merge, since, stash_password, stash_url, stash_usernam
         last_page = response_data['isLastPage']
         if not last_page:
             start = response_data['nextPageStart']
-    return jira_keys
+    return set(chain(*jira_keys))
 
 
 def open_in_browser(jira_url, jql_query, s, verbose=False):
@@ -105,21 +107,26 @@ def export_to_csv(export_csv, rows):
         f_csv.writerows(rows)
 
 
+# TODO: Inject config file in connect_to_jira
 def connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy):
     total = 0
     start_at = 0
+    # TODO: How the hell am I auto-magically gonna find out if a field is a dict, string or list? Plucking library?
+    headers = ["key", "issuetype", "status", "fixVersions", "issueLinks"]
     rows = [tuple(headers)]
     while start_at < total or total == 0:
         try:
-            # TODO: Inject config file in retrieve_jira_fields
-            # TODO: get method call out of export_to_csv
             response_data = retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_query,
                                                  proxy, start_at)
             for i in response_data["issues"]:
-                # TODO: Clean lambda from row data selection
+                # TODO: Find something to clean this mess up
+                delimiter = "/"
                 row_data = (i["key"], i["fields"]["issuetype"]["name"], i["fields"]["status"]["name"],
-                            reduce(lambda a, b: a + "," + b, [nx["name"] for nx in i["fields"]["fixVersions"] if
-                                                              len(i["fields"]["fixVersions"])]))
+                            # In case a field is a list, flatten it as a comma delimited string
+                            reduce(lambda a, b: a + delimiter + b, [nx["name"] for nx in i["fields"]["fixVersions"] if
+                                                              len(i["fields"]["fixVersions"])]),
+                            reduce(lambda a, b: a + delimiter + b, [nx["outwardIssue"]["key"] for nx in i["fields"]["issuelinks"] if
+                                                              len(i["fields"]["issuelinks"])]))
                 click.echo(row_data)
                 rows.append(row_data)
 
@@ -130,6 +137,7 @@ def connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy):
     return rows
 
 
+# TODO: Inject config file in retrieve_jira_fields
 def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_query, proxy, start_at):
     c_url = urljoin(jira_url, 'rest/api/2/search')
     data = {
@@ -143,7 +151,10 @@ def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_qu
     r = requests.post(c_url, headers=headers, data=json.dumps(data),
                       auth=HTTPBasicAuth(jira_username, jira_password),
                       proxies=get_proxy(proxy))
-    response_data = r.json()
+    if r.ok:
+        response_data = r.json()
+    else:
+        return {}
     return response_data
 
 
@@ -156,8 +167,8 @@ def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_qu
 @click.option('--jira-username', help='Username to connect to JIRA REST API')
 @click.option('--jira-password', help='Password to connect to JIRA REST API', prompt=True, hide_input=True,
               confirmation_prompt=False)
-@click.option('--project', help='Name of the project', required=True)
-@click.option('--repo', help='Name of the repository', required=True)
+@click.option('--project', help='Name of the project')
+@click.option('--repo', help='Name of the repository')
 @click.option('--since', help='Optional: Display all commits since this commit or tag')
 @click.option('--until', help='Optional: Display all commits until this branch, commit or tag')
 @click.option('--include-merge', default=False,
@@ -175,23 +186,24 @@ def main(stash_url, stash_username, stash_password, jira_url, jira_username, jir
     frame = inspect.currentframe()
     args, _, _, values = inspect.getargvalues(frame)
 
-    # config_obj = load_from_config(load_config, args, values)
-    # save_to_config(save_config, args, values)
+    click.echo("Loading config file")
+    config_obj = load_from_config(load_config, args, values)
 
-    stash_url = urljoin(stash_url, 'stash/rest/api/1.0/projects/' + project + '/repos/' + repo + '/commits')
+    click.echo("Saving config file")
+    save_to_config(save_config, config_obj)
 
-    # TODO: Inject config file in get_jira_keys
-    jira_keys = get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until)
-
-    s = set(chain(*jira_keys))
-    jql_query = 'issuekey in (' + reduce(lambda a, b: a + ", " + b, s) + ')'
+    click.echo("Connecting to Stash API")
+    jira_keys = get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until, project, repo)
+    jql_query = 'issuekey in (' + reduce(lambda a, b: a + ", " + b, jira_keys) + ')'
 
     if not skip_browser:
-        open_in_browser(jira_url, jql_query, s)
+        open_in_browser(jira_url, jql_query, jira_keys)
 
+    click.echo("Connecting to Jira API")
     rows = connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy)
 
     if export_csv:
+        click.echo("Exporting to csv")
         export_to_csv(export_csv, rows)
 
 
