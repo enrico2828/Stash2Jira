8661fa324dadbff2c33522a6f78c64b7e44775f0
enrico2828 <abeltje@gmx.net>
2015-11-22 23:12:01 +0100

    inject config files and save queries to file
    * since Jira query can only handle about 450 issues and opening in browser max 2000 characters, in case of large number of issues the script can not open them directly in the browser
    * therefor always also save the results to output file
    * output ifle contains configuration information and JIRA Jql queries
    * furthermore handle most variables through config file

--- stash2jira/cli.py
+++ stash2jira/cli.py
@@ -4,19 +4,21 @@ import inspect
 from itertools import chain
 import json
 import os
+import uuid
 import webbrowser
 from os.path import expanduser
+import sys
 
 import click as click
 import requests
 from requests.auth import HTTPBasicAuth
 from six import iteritems, viewitems
 
-from six.moves import reduce
 from six.moves.configparser import SafeConfigParser
 from six.moves.urllib.parse import urlencode, urljoin, urlparse
 
-
+MAX_JIRA_KEYS = 450
+OPEN_IN_BROWSER_BELOW = 2000
 
 # OS-independent location of home folder
 BASE_CONFIG_DIR = expanduser("~")
@@ -27,7 +29,9 @@ CONFIG_VARS = [
     {'var': "project", 'error': 'Project not specified'},
     {'var': "repo", 'error': 'Repository not specified'},
     {'var': "since", 'error': 'Start date not specified'},
-    {'var': "until", 'error': 'End date not specified'}
+    {'var': "until", 'error': 'End date not specified'},
+    {'var': "jira_username", 'error': 'JIRA username not specified'},
+    {'var': "stash_username", 'error': 'Stash username not specified'}
 ]
 
 
@@ -51,15 +55,21 @@ def load_from_config(config_file, args, values):
             for _var in CONFIG_VARS:
                 config[_var['var']] = parser.get('settings', _var['var'])
 
+    items = values.items()
+    for k, v in items:
+        if k in args and v is not None:
+            config[k] = v
+
     for _var in CONFIG_VARS:
-        if _var['var'] in args:
+        if _var['var'] in args and values[_var['var']] is not None:
             config[_var['var']] = values[_var['var']]
         if _var["var"] not in config:
             click.echo(_var['error'])
+
     return namedtuple('GenericDict', config.keys())(**config)
 
 
-def save_to_config(config_file, config_obj, verbose=False):
+def save_to_config(save_config, config_obj, verbose=False):
     """
     Write all the values in the config object to a given file in the user's home folder
 
@@ -68,14 +78,13 @@ def save_to_config(config_file, config_obj, verbose=False):
     :param verbose: Specify if stdout should display a message
     :return:
     """
-    save_config = os.path.join(BASE_CONFIG_DIR, config_file)
+
     parser = SafeConfigParser()
     parser.read(save_config)
     if not os.path.exists(save_config):
         parser.add_section('settings')
-    click.echo(config_obj)
     for k, v in viewitems(config_obj._asdict()):
-        if v is not None:
+        if v is not None and isinstance(v, str):
             parser.set('settings', k, v)
     with open(save_config, 'w') as f:
         parser.write(f)
@@ -93,64 +102,75 @@ def get_proxy(proxy_url):
     return {res.scheme: res.geturl()}
 
 
-# TODO: Inject config file in get_jira_keys
-def get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until, project, repo):
+def get_jira_keys(config_obj):
     """
     Get the issue keys from JIRA that correspond with the passed args
 
-    :param include_merge:
-    :param since:
-    :param stash_password:
-    :param stash_url:
-    :param stash_username:
-    :param until:
-    :param project:
-    :param repo:
+    :param config_obj: Config parameters from init args or user-saved config file
     :return:
     """
-    stash_url = urljoin(stash_url, 'stash/rest/api/1.0/projects/' + project + '/repos/' + repo + '/commits')
+
+    stash_url = urljoin(config_obj.stash_url,
+                        '/'.join(['stash/rest/api/1.0/projects',
+                                  config_obj.project,
+                                  'repos',
+                                  config_obj.repo,
+                                  'commits']))
     last_page = False
     start = 0
     jira_keys = list()
     while not last_page:
         params = {
-            'since': since,
-            'until': until,
+            'since': config_obj.since,
+            'until': config_obj.until,
             'start': start,
             'limit': 99999
         }
-        r = requests.get(stash_url, params=params, auth=HTTPBasicAuth(username=stash_username, password=stash_password))
+        r = requests.get(stash_url, params=params,
+                         auth=HTTPBasicAuth(username=config_obj.stash_username, password=config_obj.stash_password))
         response_data = json.loads(r.text)
-        for c in response_data['values']:
-            parents_num = len(c['parents'])
-            if 'attributes' in c.keys() and (parents_num < 2 or include_merge):
-                key_ = c['attributes']['jira-key']
-                jira_keys.append(key_)
-        last_page = response_data['isLastPage']
-        if not last_page:
-            start = response_data['nextPageStart']
-    return set(chain(*jira_keys))
+        if r.ok:
+            for c in response_data['values']:
+                parents_num = len(c['parents'])
+                if 'attributes' in c.keys() and (parents_num < 2 or config_obj.include_merge):
+                    key_ = c['attributes']['jira-key']
+                    jira_keys.append(key_)
+            last_page = response_data['isLastPage']
+            if not last_page:
+                start = response_data['nextPageStart']
+        else:
+            for e in response_data["errors"]:
+                click.echo(e["message"])
+            sys.exit(1)
+    s = set(chain(*jira_keys))
+    return s
 
 
-# TODO: Inject config object in open_in_browser
-def open_in_browser(jira_url, jql_query, jira_keys, verbose=False):
+def open_in_browser(config_obj, jql_query, query_log_path, new_tab=False):
     """
     Open browser in JIRA with the retrieved keys from Stash as url-params
 
-    :param jira_url:
+    :param config_obj:
     :param jql_query:
-    :param jira_keys:
-    :param verbose:
     :return:
     """
-    if len(jira_keys) < 500:
+
+    jira_url = config_obj.jira_url
+    if len(jql_query) < OPEN_IN_BROWSER_BELOW:
         params = {
             'jql': jql_query
         }
         b_url = urljoin(urljoin(jira_url, 'issues/'), '?' + urlencode(params))
-        webbrowser.open(b_url)
+        if new_tab:
+            webbrowser.open(b_url, new=2)
+        else:
+            webbrowser.open(b_url, new=0)
     else:
-        click.echo("Too much data to handle in browser")
+        click.echo("Too much data to open in browser.")
+
+    click.echo("Query saved to " + query_log_path)
+    with open(query_log_path, 'a') as f:
+        f.write(jql_query + "\n\n")
 
 
 # TODO: Inject config object in export_to_csv
@@ -174,19 +194,22 @@ def find(key, dictionary):
                     yield result
 
 
-# TODO: Inject config file in connect_to_jira
-def connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy, verbose=False):
+def connect_to_jira(config_file, jql_query):
     """
     Connect to JIRA's REST API and parse the given JQL-query
 
-    :param jira_password:
-    :param jira_url:
-    :param jira_username:
+    :param config_file:
     :param jql_query:
-    :param proxy:
     :return:
     """
     # TODO: Seriously, implement the jira python package: https://pypi.python.org/pypi/jira
+
+    jira_password = config_file["jira_password"]
+    jira_url = config_file['jira_url']
+    jira_username = config_file['jira_username']
+    proxy = config_file['proxy']
+    verbose = config_file['verbose']
+
     total = 0
     start_at = 0
     # TODO: Use this: http://funcy.readthedocs.org/en/latest/colls.html#get_in
@@ -213,8 +236,6 @@ def connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy, ve
 # TODO: Inject config file in retrieve_jira_fields
 def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_query, proxy, start_at):
     """
-
-
     :param headers:
     :param jira_password:
     :param jira_url:
@@ -245,7 +266,7 @@ def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_qu
 
 @click.command()
 @click.option('--stash-url', help='URL to Stash REST API to connect to, e.g. http://stash.example.com/stash')
-@click.option('--stash-username', help='Username to connect to Stash REST API', required=True)
+@click.option('--stash-username', help='Username to connect to Stash REST API')
 @click.option('--stash-password', help='Password to connect to Stash REST API', prompt=True, hide_input=True,
               confirmation_prompt=False)
 @click.option('--jira-url', help='Base URL for output link, e.g. https://jira.example.com')
@@ -259,33 +280,16 @@ def retrieve_jira_fields(headers, jira_password, jira_url, jira_username, jql_qu
 @click.option('--include-merge', default=False,
               help='Optional: Whether merge commits should be included or not. Defaults to false', type=bool)
 @click.option('--save-config',
-              help="Specifiy the configuration file the given command line parameters should be saved to in the user's home directory")
+              help="Specify the configuration file the given command line parameters should be "
+                   "saved to in the user's home directory")
 @click.option('--load-config',
               help="Specifying the name of the config file to save/read. Given parameters override the config file.")
 @click.option('--export-csv', help='Specify a csv-file to export to')
-@click.option('--skip-browser', default=True,
+@click.option('--skip-browser', default=False,
               help="Whether to open the JIRA dashboard with the results in the browser. Defaults to true", type=bool)
 @click.option('--proxy', help='Specify which proxy to connect through e.g. https://proxy.url')
 def main(stash_url, stash_username, stash_password, jira_url, jira_username, jira_password, project, repo, since,
          until, include_merge, save_config, load_config, export_csv, skip_browser, proxy):
-    """
-    :param stash_url: URL to Stash REST API to connect to, e.g. http://stash.example.com/stash
-    :param stash_username: Username to connect to Stash REST API
-    :param stash_password: Password to connect to Stash REST API
-    :param jira_url: Base URL for output link, e.g. https://jira.example.com
-    :param jira_username: Username to connect to JIRA REST API
-    :param jira_password: Password to connect to JIRA REST API
-    :param project: Name of the project
-    :param repo: Name of the repository
-    :param since: Optional: Display all commits since this commit or tag
-    :param until: Optional: Display all commits until this branch, commit or tag
-    :param include_merge: Optional: Whether merge commits should be included or not. Defaults to false
-    :param save_config: Specifiy the configuration file the given command line parameters should be saved to in the user's home directory
-    :param load_config: Specifiy the configuration file the given command line parameters should be saved to in the user's home directory
-    :param export_csv: Specify a csv-file to export to
-    :param skip_browser: Whether to open the JIRA dashboard with the results in the browser. Defaults to true
-    :param proxy: Specify which proxy to connect through e.g. https://proxy.url
-    """
 
     # get the arguments and values from main in arrays
     frame = inspect.currentframe()
@@ -295,27 +299,29 @@ def main(stash_url, stash_username, stash_password, jira_url, jira_username, jir
     config_obj = load_from_config(load_config, args, values)
 
     click.echo("Saving config file")
-    save_to_config(save_config, config_obj)
+    if save_config:
+        save_config = os.path.join(BASE_CONFIG_DIR, save_config)
+        save_to_config(save_config, config_obj)
 
     click.echo("Connecting to Stash API")
-    jira_keys = get_jira_keys(include_merge, since, stash_password, stash_url, stash_username, until, project, repo)
+    jira_keys = list(get_jira_keys(config_obj))
+    t_amount = len(jira_keys) / MAX_JIRA_KEYS
 
     if not skip_browser:
+        query_log = os.path.join(BASE_CONFIG_DIR, 'jql-query' + str(uuid.uuid4()) + '.log')
+        save_to_config(query_log, config_obj)
         click.echo("Opening results in browser...")
-        temp_jira_keys = list()
-        counter = 0
-        for i in jira_keys:
-          counter = counter + 1
-          temp_jira_keys.append(i)
-          maxissues = len(jira_keys)
-          if len(temp_jira_keys) == 150 or counter == maxissues:
-                    jql_query = 'issuekey in (' + reduce(lambda a, b: a + ", " + b, temp_jira_keys) + ')'
-                    open_in_browser(jira_url, jql_query, temp_jira_keys)
-                    del temp_jira_keys[:]
+        for t in range(t_amount + 1):
+            t_ = jira_keys[(MAX_JIRA_KEYS * t):(MAX_JIRA_KEYS * (t + 1))]
+            jql_query = 'issuekey in (' + ", ".join(t_) + ')'
+            if t == 0:
+                open_in_browser(config_obj, jql_query, query_log_path=query_log)
+            else:
+                open_in_browser(config_obj, jql_query, query_log_path=query_log, new_tab=False)
 
     if export_csv:
         click.echo("Connecting to Jira API")
-        rows = connect_to_jira(jira_password, jira_url, jira_username, jql_query, proxy)
+        rows = connect_to_jira(config_obj, jql_query)
 
         click.echo("Exporting to csv")
         export_to_csv(export_csv, rows)
